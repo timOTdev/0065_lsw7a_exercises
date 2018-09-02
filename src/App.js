a68de407c583825681badd9d10d01e28a0d5d676
Timothy Hoang <timh1203@gmail.com>
2018-09-02 17:04:29 -0400

    Add constructor and animal file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      animasl: []
+    }
+  }
+  
   render() {
     return (
       <div className="App">
